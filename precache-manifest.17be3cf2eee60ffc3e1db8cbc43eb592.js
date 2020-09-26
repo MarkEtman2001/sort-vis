4321104d6ce88b553c0cddc26e08f7ea06c3f23d
Mark Etman <marketman@Marks-MacBook-Pro.local>
2020-09-26 16:50:32 +0100

    Updates

---
+++ precache-manifest.17be3cf2eee60ffc3e1db8cbc43eb592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f22bfb6ec65a6d23e5617944fbbf45",
+    "url": "/sort-vis/index.html"
+  },
+  {
+    "revision": "3d80e0abf1cd60767d9d",
+    "url": "/sort-vis/static/css/main.1c3c0852.chunk.css"
+  },
+  {
+    "revision": "58b8a36935d3d2d84f2c",
+    "url": "/sort-vis/static/js/2.5f61be26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sort-vis/static/js/2.5f61be26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d80e0abf1cd60767d9d",
+    "url": "/sort-vis/static/js/main.ba4fc203.chunk.js"
+  },
+  {
+    "revision": "e1eab5a723ccb230dccf",
+    "url": "/sort-vis/static/js/runtime-main.6e8ca2f8.js"
+  }
+]);
